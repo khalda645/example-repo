90be24c00029cb6d4345c0f44c8cdf39f8ee90e5
khalda645 <khaldasatti6@gmail.com>
2025-07-03 18:12:31 +0100

    Delete ifExample.ps1

--- ifExample.ps1
+++
@@ -1,2 +0,0 @@
-
-This repo contains the ps script for creating a new folder if one doesn't exist and if the newly created one exists, a folder called hyperion is created. Otherwise, is called newprojects.
